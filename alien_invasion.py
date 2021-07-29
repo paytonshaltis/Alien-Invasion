24d7a257a1ac94e46397a6280a6e65d69a9a82d1
Payton Shaltis <shaltisp@gmail.com>
2021-07-29 01:04:52 -0400

    ship now moves, won't move off of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
+from os import CLD_TRAPPED
 import sys
-import pygame.display, pygame.event
+import pygame, pygame.display, pygame.event
 from pygame.locals import *
 
 from settings import Settings
@@ -8,12 +9,12 @@ from ship import Ship
 class AlienInvasion:
     """Overall class that will manage game behavior."""
 
-    def __init__(self):
+    def __init__(self, screen):
         """Initialize the game and create needed resources."""
         pygame.init()
         
         # create a settings attribute for this game instance
-        self.settings = Settings('MacBook Pro')
+        self.settings = Settings(screen)
 
         # set the screen dimensions and caption (this is a surface)
         self.screen = pygame.display.set_mode(
@@ -23,23 +24,55 @@ class AlienInvasion:
         # create a ship attribute for this game instance
         self.ship = Ship(self)
 
+
     def run_game(self):
         """Starts the main game loop."""
         while True:
-            # watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
 
-            # redraw the ship during each pass through the loop
-            self.ship.blitme()
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
 
-            # make the most recently drawn screen visible
-            pygame.display.flip()
+            # closing the window quits the game
+            if event.type == QUIT:
+                sys.exit()
+
+            # check certain event pased on event.type
+            elif event.type == KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == K_q:
+            sys.exit()
+
+
+    def _check_keyup_events(self, event):
+        if event.key == K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.update()
+        self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
-    game = AlienInvasion()
+    game = AlienInvasion('AOC')
     game.run_game()
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initialize the game's settings. Screen determines
            the resolution of the game based on screentype."""
         
-        # determine multiplier
+        # determine multiplier for screen dimensions
         if screen == 'MacBook Pro':
             multiplier = 0.75
         elif screen == 'AOC':
@@ -14,3 +14,6 @@ class Settings:
         self.screen_width = int(1200 * multiplier)
         self.scren_height = int(800 * multiplier)
         self.bg_color = (230, 230, 230)
+
+        # ship settings
+        self.ship_speed = 1.5
